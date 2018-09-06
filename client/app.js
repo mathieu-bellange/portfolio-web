f1ed31635b7f0af9b4b029f31f517c3140b31e91
mbellange <mbellange@gmail.com>
2018-09-06 14:58:18 +0200

    smooth scrolling
    - ajout d'un scrolling smooth en js

--- client/app.js
+++ client/app.js
@@ -1,2 +1,24 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './app.css';
+
+const scroll = (eventTarget) => {
+  if (window.location.pathname.replace(/^\//,'') == eventTarget.pathname.replace(/^\//,'') && window.location.hostname == eventTarget.hostname) {
+      const elementTarget = document.querySelector(eventTarget.hash);
+      if (elementTarget) {
+        event.preventDefault();
+        window.location.hash = eventTarget.hash;
+        document.querySelector('.container').scroll({
+          top: elementTarget.offsetTop,
+          left: 0,
+          behavior: 'smooth'
+        });
+        elementTarget.focus({ preventScroll: true });
+      }
+    }
+};
+
+document.querySelector('a[href*="#"]:not([href="#"])').addEventListener('click', (event) => {
+  scroll(event.currentTarget);
+});
+
+document.addEventListener('DOMContentLoaded', () => scroll(document.querySelector(`a[href="${window.location.hash}"]`)));
