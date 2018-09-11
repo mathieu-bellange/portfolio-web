8b4166a5b20cf2e4142c8261de3767b828ddd2bc
mbellange <mbellange@gmail.com>
2018-09-11 10:50:12 +0200

    fix webpack prod
    - correctif sur le plugin webpack uglifyjs

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const commonConfig = require('./webpack.common.js'); // the settings that are co
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const CompressionPlugin = require('compression-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -117,40 +117,42 @@ module.exports = webpackMerge(commonConfig, {
      */
     // NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
     new UglifyJsPlugin({
-      // beautify: true, //debug
-      // mangle: false, //debug
-      // dead_code: false, //debug
-      // unused: false, //debug
-      // deadCode: false, //debug
-      // compress: {
-      //   screw_ie8: true,
-      //   keep_fnames: true,
-      //   drop_debugger: false,
-      //   dead_code: false,
-      //   unused: false
-      // }, // debug
-      // comments: true, //debug
+      uglifyOptions: {
+        // beautify: true, //debug
+        // mangle: false, //debug
+        // dead_code: false, //debug
+        // unused: false, //debug
+        // deadCode: false, //debug
+        // compress: {
+        //   ie8: false,
+        //   keep_fnames: true,
+        //   drop_debugger: false,
+        //   dead_code: false,
+        //   unused: false
+        // }, // debug
+        // comments: true, //debug
 
 
-      beautify: false, // prod
-      output: {
-        comments: false
-      }, // prod
-      mangle: {
-        screw_ie8: true
-      }, // prod
-      compress: {
-        screw_ie8: true,
-        warnings: false,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-        negate_iife: false // we need this for lazy v8
+        beautify: false, // prod
+        output: {
+          comments: false
+        }, // prod
+        mangle: {
+          ie8: false
+        }, // prod
+        compress: {
+          ie8: false,
+          warnings: false,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+          negate_iife: false // we need this for lazy v8
+        }
       }
     }),
 
